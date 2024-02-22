694ffe9dc61cb18f8962a8161bb348106201f7f1
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-02-22 12:02:31 +0530

    Update and_gate_tb.v

--- Verilog/Day 1/and_gate_tb.v
+++ Verilog/Day 1/and_gate_tb.v
@@ -1,1 +1,26 @@
-
+// Code your testbench here
+// or browse Examples
+module and_gate_tb;
+  reg a,b;
+  wire y;  
+//instantiate the AND gate module
+  and_gate andgate(.A(a),.B(b),.Y(y));
+//dump waveform information
+  initial begin
+    $dumpfile("and_gate.vcd");
+    $dumpvars(1, and_gate_tb);
+    $monitor("a=%0d b=%0d y=%0d",a,b,y);
+  end
+//apply input values
+  initial begin
+    a=0 ; b=0;
+    #10
+    a=0 ; b=1;
+    #10 
+    a=1 ; b=0;
+    #10 
+    a=1 ; b=1;
+    #10
+    $finish;
+  end
+endmodule
