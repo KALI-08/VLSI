2e83c87d64385ed33cae73b32b8698c7f860bd45
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-02-27 15:21:16 +0530

    Create t_ff.v

---
+++ Verilog/Day 5/t_ff.v
@@ -0,0 +1,16 @@
+module t_ff(
+  input t,
+  input clk,
+  output q,
+ reg q);
+  initial
+    q=0;
+  always @(posedge clk)
+    begin 
+    
+      if(t==1)
+      q=~q;
+      else if(t==0)
+        q=q;
+    end
+endmodule
