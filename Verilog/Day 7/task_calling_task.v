2a1e8859f9fb300e3ebef9c5330e8e6d44abf73f
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-02-29 14:45:07 +0530

    Create task_calling_task.v

---
+++ Verilog/Day 7/task_calling_task.v
@@ -0,0 +1,26 @@
+task add (input [7:0]a,b,output int c);
+  begin
+    c=a+b;
+    $display("c=%0d",c);
+  end
+endtask
+task multi (input [7:0]a,b,output int c);
+      begin
+        c=a*b;
+        $display("c=%0d",c);
+      end
+endtask
+task arithmetic;
+  output int c;
+ multi(5,7,c);
+    add(4,7,c);
+endtask
+
+module sum;
+  int c;
+  initial begin
+   
+   // arithmetic;
+    add(4,7,c);
+  end
+endmodule
