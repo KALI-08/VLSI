b67a68d1caee1fb713cc01b458d5f568c52929f3
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-03-06 10:33:57 +0530

    Create up_counter_tb.v

---
+++ Verilog/Day 12/up_counter_tb.v
@@ -0,0 +1,16 @@
+module up_counter_tb;
+  reg clk,reset;
+  wire [7:0]out;
+  up_counter up_counter(.clk(clk),.reset(reset),.out(out));
+  initial begin
+    reset=1;
+    clk=0;
+    #5 reset=0;
+    #200
+    $finish;
+  end
+  initial begin
+    $monitor("t=%d reset=%b clk=%b out=%d",$time,reset,clk,out);
+  end
+  always #5 clk=~clk;
+endmodule
