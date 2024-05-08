b0d8a9c1236f32773282535ff6925409f177dfe3
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-05-08 08:58:07 +0530

    Update implication.sv

--- System Verilog/Constraint/Implication/implication.sv
+++ System Verilog/Constraint/Implication/implication.sv
@@ -1,1 +1,19 @@
+class example;
+  rand bit [3:0] data;
+  rand bit [3:0] value;
+  
+  constraint c1{(value>10)->data<3;}
+  
+endclass
 
+module implication;
+  example ex;
+  initial begin
+    ex=new();
+    repeat(15) begin
+      ex.randomize();
+      $display("--------------------------");
+      $display("data=%0d  value=%0d",ex.data,ex.value);
+    end
+  end
+endmodule
