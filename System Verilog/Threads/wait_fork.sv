0203bcacea75afd22a88e2474c5157d10151e515
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-05-15 11:18:32 +0530

    Create wait_fork.sv

---
+++ System Verilog/Threads/wait_fork.sv
@@ -0,0 +1,27 @@
+module fork_join;
+  initial begin
+    $display("------------------------------");
+    fork
+      begin
+      $display("t=%0d process 1 started",$time);
+      #5
+      $display("t=%0d process 1 finished",$time);
+      end
+      begin
+      $display("t=%0d process 2 started",$time);
+      #10
+      $display("t=%0d process 2 finished",$time);
+      end
+      begin
+      $display("t=%0d process 3 started",$time);
+      #20
+      $display("t=%0d process 3 finished",$time);
+      end
+    join_none
+    wait fork;
+    $display("***************************");
+    $display("*  fork...join completed  *");
+    $display("***************************");
+  end
+endmodule
+        
