da1b271668b2606ab46ac06fd69f9c20d7fbdb71
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-06-26 12:33:17 +0530

    Create case_without_default.sv

---
+++ System Verilog/Control Flow/Conditional Statement/case_without_default.sv
@@ -0,0 +1,12 @@
+module ex_case;
+  bit[1:0]x;
+  initial begin
+    x=2'b01;
+    case(x)
+      00: $display("case 00 value of x=%0d",x);
+      01: $display("case 01 value of x=%0d",x);
+      10: $display("case 10 value of x=%0d",x);
+      11: $display("case 11 value of x=%0d",x);
+    endcase
+  end
+endmodule
