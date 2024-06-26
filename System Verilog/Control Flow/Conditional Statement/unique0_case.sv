f0f5859d77751df6d0c60871e60566dffe9bd91c
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-06-26 14:29:06 +0530

    Create unique0_case.sv

---
+++ System Verilog/Control Flow/Conditional Statement/unique0_case.sv
@@ -0,0 +1,13 @@
+module unique0_case;
+  int x;
+  initial begin
+    x=2'b01;
+    unique0 case(x)
+      00: $display("1) value of x is=%0d",x);
+      01: $display("1) value of x is=%0d",x);
+      10: $display("1) value of x is=%0d",x);
+      11: $display("1) value of x is=%0d",x);
+      01: $display("1) value of x is=%0d",x);
+    endcase
+  end
+endmodule
