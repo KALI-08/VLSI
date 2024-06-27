ce38654efcf6cd124a182fa88e3b5e5941a35df6
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-06-27 11:03:43 +0530

    Create class_object.sv

---
+++ System Verilog/Class/Class_Object/class_object.sv
@@ -0,0 +1,26 @@
+class room;
+  bit light;
+  int fan;
+  string switch;
+  task electricity;
+    switch="OFF";
+    $display("power supply is off");
+  endtask
+endclass
+  
+  module hotel;
+    room r1;
+    initial begin
+      
+      $display("handle is created");
+      r1=new();
+      $display("object is created");
+      $display("lets check obeject is created or not");
+      if(r1==null) begin
+        $display("object is empty");
+      end 
+      else begin
+        $display("object is full");
+      end
+    end
+  endmodule
