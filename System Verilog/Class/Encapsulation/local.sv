e40f9f2dbaacbc55a415f772925a7aaf8d605b74
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2024-08-31 17:07:29 +0530

    Create local.sv

---
+++ System Verilog/Class/Encapsulation/local.sv
@@ -0,0 +1,26 @@
+class A;
+  local bit [3:0]a;
+  bit [3:0]b;
+  
+  function display1();
+    a=4; b=10;
+    $display("a--%0d b--%0d",a,b);
+  endfunction
+endclass
+
+class B extends A;
+  function display();
+    //a=15;
+    b=3;
+    $display("b--%0d",b);
+  endfunction
+endclass
+
+module ex;
+  B bb;
+  initial begin
+    bb=new();
+    bb.display();
+    bb.display1();
+  end
+endmodule
