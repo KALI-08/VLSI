7fa17e438943b30333f72fcf6bca0051409bf0b4
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2025-01-07 08:53:59 +0530

    Create monitor.sv

---
+++ Debugging Assignment/ALU_SV/monitor.sv
@@ -0,0 +1,10 @@
+module monitor (alu_if mon_if);
+  
+    task monitor_output(alu_transaction txn);
+        txn.a = mon_if.a;
+        txn.b = mon_if.b;
+        txn.op = mon_if.op;
+        txn.expected_result = mon_if.result;
+        #10;
+    endtask
+endmodule
