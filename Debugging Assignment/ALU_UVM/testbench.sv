64b861c62b877f6f7b50248d6e1f0693800f6241
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2025-01-07 09:03:52 +0530

    Create testbench.sv

---
+++ Debugging Assignment/ALU_UVM/testbench.sv
@@ -0,0 +1,29 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+`include "interface.sv"
+`include "alu_transaction.sv"
+//`include "sequence.sv"
+//`include "sequencer.sv"
+`include "alu_driver.sv"
+`include "alu_monitor.sv"
+`include "alu_env.sv"
+`include "alu_test.sv"
+
+//`include "agent.sv"
+module tb_top_module;
+  alu_test test;
+  alu_if vintf();
+    alu dut (
+        .a(vintf.a),
+        .b(vintf.b),
+        .op(vintf.op),
+        .result(vintf.result)
+    );
+
+initial begin
+  uvm_config_db#(virtual alu_if)::set(null,"","vintf",vintf);
+    run_test("alu_test");
+end
+
+endmodule
+
