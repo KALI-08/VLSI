7c516bfe9b50eb824a40af71b39a52ea3cfa0230
KALI-08 <130536613+KALI-08@users.noreply.github.com>
2025-01-07 08:55:53 +0530

    Create test.sv

---
+++ Debugging Assignment/ALU_SV/test.sv
@@ -0,0 +1,13 @@
+module test;
+   alu_transaction txn;
+   
+    initial begin
+       
+        for (int i = 0; i < 10; i++) begin
+            txn = new();
+            txn.randomize();
+            env.run(txn);
+        end
+        $finish;
+    end
+endmodule
